296ec45f2c31ce9741a5a5c6ade5269d240cc9bd
nydemeth <20528801+nydemeth@users.noreply.github.com>
2021-05-02 20:10:12 +0200

    db init

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+include 'config.php';
 
-var_dump($_REQUEST);
+var_dump($_SERVER);
+
+$pdo = new PDO('mysql:host='.$sqlhost.';dbname='.$sqldb, $sqluser, $sqlpw);
+
+$short = substr($_SERVER['REDIRECT_URL'],1);
+
+$statement = $pdo->prepare("SELECT * FROM urls WHERE short = ?");
+$statement->execute(array($short));   
+$row = $statement->fetch();
+var_dump($row);
 
 ?>
